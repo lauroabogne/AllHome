db2514068ff944fca0b5c5a256da33e758ea0427
Lauro Abogne Jr. <lauroabogne@gmail.com>
2024-04-14 15:50:40 +0800

    Created centrall code for viewing image

--- app/src/main/java/com/example/allhome/utils/ImageUtil.kt
+++ app/src/main/java/com/example/allhome/utils/ImageUtil.kt
@@ -1,16 +1,26 @@
 package com.example.allhome.utils
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.graphics.Matrix
+import android.graphics.Point
+import android.graphics.Rect
+import android.graphics.RectF
 import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.view.animation.DecelerateInterpolator
 import com.example.allhome.grocerylist.GroceryUtil
+import com.github.chrisbanes.photoview.PhotoView
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -199,6 +209,177 @@ class ImageUtil {
         fun doImageExist(imagePath:String): Boolean {
             return File(imagePath).exists()
         }
+
+        fun zoomInImageFromThumb(thumbView: View, imageUri: Uri, mainLayout: View, imageContainer: View, imageHolder: PhotoView, animationDuration:Long) {
+
+            imageHolder.setImageURI(imageUri)
+
+            // Calculate the starting and ending bounds for the zoomed-in image.
+            // This step involves lots of math. Yay, math.
+            val startBoundsInt = Rect()
+            val finalBoundsInt = Rect()
+            val globalOffset = Point()
+
+            // The start bounds are the global visible rectangle of the thumbnail,
+            // and the final bounds are the global visible rectangle of the container
+            // view. Also set the container view's offset as the origin for the
+            // bounds, since that's the origin for the positioning animation
+            // properties (X, Y).
+            thumbView.getGlobalVisibleRect(startBoundsInt)
+            mainLayout.getGlobalVisibleRect(finalBoundsInt, globalOffset)
+            startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+            finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+            val startBounds = RectF(startBoundsInt)
+            val finalBounds = RectF(finalBoundsInt)
+
+            // Adjust the start bounds to be the same aspect ratio as the final
+            // bounds using the "center crop" technique. This prevents undesirable
+            // stretching during the animation. Also calculate the start scaling
+            // factor (the end scaling factor is always 1.0).
+            val startScale: Float
+            if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+                // Extend start bounds horizontally
+                startScale = startBounds.height() / finalBounds.height()
+                val startWidth: Float = startScale * finalBounds.width()
+                val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+                startBounds.left -= deltaWidth.toInt()
+                startBounds.right += deltaWidth.toInt()
+            } else {
+                // Extend start bounds vertically
+                startScale = startBounds.width() / finalBounds.width()
+                val startHeight: Float = startScale * finalBounds.height()
+                val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+                startBounds.top -= deltaHeight.toInt()
+                startBounds.bottom += deltaHeight.toInt()
+            }
+
+            // Hide the thumbnail and show the zoomed-in view. When the animation
+            // begins, it will position the zoomed-in view in the place of the
+            // thumbnail.
+            thumbView.alpha = 0f
+            imageContainer.visibility = View.VISIBLE
+
+            // Set the pivot point for SCALE_X and SCALE_Y transformations
+            // to the top-left corner of the zoomed-in view (the default
+            // is the center of the view).
+            imageContainer.pivotX = 0f
+            imageContainer.pivotY = 0f
+
+            // Construct and run the parallel animation of the four translation and
+            // scale properties (X, Y, SCALE_X, and SCALE_Y).
+            var currentAnimator: AnimatorSet? = AnimatorSet().apply {
+                play(
+                    ObjectAnimator.ofFloat(
+                        imageContainer,
+                        View.X,
+                        startBounds.left,
+                        finalBounds.left
+                    )
+                ).apply {
+                    with(ObjectAnimator.ofFloat(imageContainer, View.Y, startBounds.top, finalBounds.top))
+                    with(ObjectAnimator.ofFloat(imageContainer, View.SCALE_X, startScale, 1f))
+                    with(ObjectAnimator.ofFloat(imageContainer, View.SCALE_Y, startScale, 1f))
+                }
+                duration = animationDuration
+                interpolator = DecelerateInterpolator()
+            }
+
+            currentAnimator?.addListener(object : AnimatorListenerAdapter(){
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            currentAnimator?.start()
+
+        }
+
+
+        fun zoomOutImageFromThumb(thumbView: View, mainLayout:View,  imageContainer:View,  animationDuration:Long) {
+
+            // If there's an animation in progress, cancel it
+            // immediately and proceed with this one.
+            // currentAnimator?.cancel()
+
+            // val imageContainer = dataBindingUtil.imageContainerIncludedLayout.imageContainer
+
+            // Calculate the starting and ending bounds for the zoomed-in image.
+            // This step involves lots of math. Yay, math.
+            val startBoundsInt = Rect()
+            val finalBoundsInt = Rect()
+            val globalOffset = Point()
+
+            // The start bounds are the global visible rectangle of the thumbnail,
+            // and the final bounds are the global visible rectangle of the container
+            // view. Also set the container view's offset as the origin for the
+            // bounds, since that's the origin for the positioning animation
+            // properties (X, Y).
+            thumbView.getGlobalVisibleRect(startBoundsInt)
+            mainLayout.getGlobalVisibleRect(finalBoundsInt, globalOffset)
+            startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+            finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+            val startBounds = RectF(startBoundsInt)
+            val finalBounds = RectF(finalBoundsInt)
+
+            // Adjust the start bounds to be the same aspect ratio as the final
+            // bounds using the "center crop" technique. This prevents undesirable
+            // stretching during the animation. Also calculate the start scaling
+            // factor (the end scaling factor is always 1.0).
+            val startScale: Float
+            if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+                // Extend start bounds horizontally
+                startScale = startBounds.height() / finalBounds.height()
+                val startWidth: Float = startScale * finalBounds.width()
+                val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+                startBounds.left -= deltaWidth.toInt()
+                startBounds.right += deltaWidth.toInt()
+            } else {
+                // Extend start bounds vertically
+                startScale = startBounds.width() / finalBounds.width()
+                val startHeight: Float = startScale * finalBounds.height()
+                val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+                startBounds.top -= deltaHeight.toInt()
+                startBounds.bottom += deltaHeight.toInt()
+            }
+
+
+            // Upon clicking the zoomed-in image, it should zoom back down
+            // to the original bounds and show the thumbnail instead of
+            // the expanded image.
+
+
+            // Animate the four positioning/sizing properties in parallel,
+            // back to their original values.
+            var currentAnimator : AnimatorSet? = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(imageContainer, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(imageContainer, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(imageContainer, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(imageContainer, View.SCALE_Y, startScale))
+                }
+                duration = animationDuration
+                interpolator = DecelerateInterpolator()
+            }
+
+            currentAnimator?.addListener(object:AnimatorListenerAdapter(){
+                override fun onAnimationEnd(animation: Animator) {
+                    thumbView.alpha = 1f
+                    imageContainer.visibility = View.GONE
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    thumbView.alpha = 1f
+                    imageContainer.visibility = View.GONE
+                    currentAnimator = null
+                }
+            })
+            currentAnimator?.start()
+        }
+
     }
 
 }
